a278d090af9c9d51a5d002a786fbced789ee1dd2
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2019-12-26 17:35:46 +0100

    Mejora1: first try. Running but should throw ConcurrentModificationException and it does not

--- src/test/java/es/sidelab/webchat/ChatManagerTest.java
+++ src/test/java/es/sidelab/webchat/ChatManagerTest.java
@@ -2,6 +2,7 @@ package es.sidelab.webchat;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collection;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -64,5 +65,50 @@ public class ChatManagerTest {
 		assertTrue("Notified new user '" + newUser[0] + "' is not equal than user name 'user2'",
 				"user2".equals(newUser[0]));
 
+	}
+	
+	@Test
+	public void mejora1() throws InterruptedException, TimeoutException {
+		
+		final int NUM_CONCURRENT_USERS = 4;
+		final int MAX_CHATS = 50;
+
+		ChatManager chatManager = new ChatManager(MAX_CHATS);
+		
+		final Thread[] threadsList = new Thread[NUM_CONCURRENT_USERS];
+		
+		for (int i = 0; i < NUM_CONCURRENT_USERS; i++) {
+			threadsList[i] = new Thread(mejora1Thread(chatManager, i));
+		}
+		
+		for (int i = 0; i < NUM_CONCURRENT_USERS; i++) {
+			threadsList[i].start();
+		}
+
+		for (int i = 0; i < NUM_CONCURRENT_USERS; i++) {
+			threadsList[i].join();
+		}
+	}
+	
+	public Runnable mejora1Thread(ChatManager chatManager, int userIndex) throws InterruptedException, TimeoutException {
+		
+		Thread.sleep((long)(Math.random() * 1));
+
+		TestUser user = new TestUser("user" + userIndex);
+		chatManager.newUser(user);
+				
+		final int NUM_ITERATIONS = 4;
+		
+		for (int userIteration = 0; userIteration < NUM_ITERATIONS; userIteration++) {
+			System.out.println("Running..." + " userIndex #" + userIndex + " userIteration #" + userIteration);
+
+			Chat chat = chatManager.newChat("Chat" + userIteration, 5, TimeUnit.SECONDS);
+			
+			chat.addUser(user);
+			
+			Collection<User> usersInChat = chat.getUsers();
+			System.out.println(usersInChat.toString());			
+		}
+		return null;
 	}
 }
