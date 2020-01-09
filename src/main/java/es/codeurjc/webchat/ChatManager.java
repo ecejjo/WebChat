66b4de40a23cc337fdc8ddb28e198e76546f648d
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2020-01-10 00:42:54 +0100

    mejora4_1_ChatManager_newChat now working.

--- src/main/java/es/codeurjc/webchat/ChatManager.java
+++ src/main/java/es/codeurjc/webchat/ChatManager.java
@@ -1,8 +1,15 @@
 package es.codeurjc.webchat;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -14,6 +21,8 @@ public class ChatManager {
 	
 	private int maxChats;	
 	private final Semaphore maxChatsSemaphore;
+	
+	private CountDownLatch newChatLatch = new CountDownLatch(0);
 	
 	public ChatManager(int maxChats) {
 		this.maxChats = maxChats;
@@ -41,9 +50,7 @@ public class ChatManager {
 				// the previous value associated with the specified key,
 				// or null if there was no mapping for the key
 				if (chats.putIfAbsent(name, newChat) == null) {
-					for(User user : users.values()){
-						user.newChat(newChat);
-					}
+					notifyUsersNewChat(newChat);
 					return newChat;
 				}
 				// Chat already existed
@@ -61,6 +68,46 @@ public class ChatManager {
 		{
 			throw new TimeoutException("InterruptedException.");
 		}
+	}
+	
+	private void notifyUsersNewChat(Chat newChat) throws InterruptedException, TimeoutException {
+		
+		// Using a copy of users in chat to avoid concurrency problems.
+		ArrayList<User> usersInChat = new ArrayList<>(users.values());
+		
+		ExecutorService executorService = Executors.newFixedThreadPool(usersInChat.size());
+		CompletionService<Boolean> completionService = new ExecutorCompletionService<>(executorService);
+		
+		// CountDownLatch must be created before any submit to avoid race conditions
+		newChatLatch = new CountDownLatch(usersInChat.size());
+		
+		for (int i = 0; i < usersInChat.size(); i++) {			
+			final int userIndex = i;
+			completionService.submit(() -> newChatThread(usersInChat.get(userIndex), newChat));
+		}
+		
+		try {
+			newChatLatch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Checks execution results (futures)
+		for (int i = 0; i < usersInChat.size(); i++) {
+			try {
+				Future<Boolean> f = completionService.take();
+				assert(f.get().equals(true));
+			} catch (Exception e) {
+				System.out.println("Thread execution throwed exception: " + e.getMessage());
+			}
+		}
+	}
+	
+	private boolean newChatThread(User user, Chat newChat) {
+		user.newChat(newChat);
+		newChatLatch.countDown();
+		return true;
 	}
 
 	public void closeChat(Chat chat) {
